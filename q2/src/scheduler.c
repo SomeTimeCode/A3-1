7adedc9d9104b4bcd3c7c37ec9a5e104956a92ff
SomeTimeCode <69341768+SomeTimeCode@users.noreply.github.com>
2020-12-05 10:57:37 +0800

    Add files via upload

---
+++ q2/src/scheduler.c
@@ -0,0 +1,84 @@
+#include "../include/scheduler.h"
+
+void *simpleRobotRoutine(void *arg) {
+  Robot robot = (Robot)arg;
+  Task task = robot->task;
+#ifdef DEBUG
+    debug_printf(__func__, "Robot%c[%d] starts...\n", 
+        RobotTypeToChar(robot->robotType), robot->id);
+#endif 
+  int jobID;
+  while (!queueIsEmpty(task->jobQ)) {
+    if(queueIsEmpty(task->jobQ)){
+      break;
+    }else if((*queueFront(task->jobQ) == 0||*queueFront(task->jobQ) == 1||*queueFront(task->jobQ) == 2) && (task->bodyIsHere) < 1){
+      printf("const char *restrict __format, ...");
+      continue;
+    }
+    queueDequeueFront(task->jobQ, &jobID);
+#ifdef DEBUG
+    debug_printf(__func__, "Robot%c[%d]: working on job %d...\n", 
+        RobotTypeToChar(robot->robotType), robot->id, jobID);
+#endif
+    simpleWork(jobID, robot);
+  }
+  pthread_exit(NULL);
+}
+
+void simpleWork(int jobID, Robot robot) {
+  Task task = robot->task;
+  double timer = getCurrentTime();
+  switch (jobID) {
+  case SKELETON:
+#ifdef DEBUG
+    debug_printf(__func__, "Robot%c[%d] making skeleton...\n", 
+        RobotTypeToChar(robot->robotType), robot->id);
+    fflush(stdout);
+#endif
+    makeSkeleton(robot);
+    break;
+  case ENGINE:
+#ifdef DEBUG
+    debug_printf(__func__, "Robot%c[%d] making engine...\n", 
+        RobotTypeToChar(robot->robotType), robot->id);
+    fflush(stdout);
+#endif
+    makeEngine(robot);
+    break;
+  case CHASSIS:
+#ifdef DEBUG
+    debug_printf(__func__, "Robot%c[%d] making chassis...\n", 
+        RobotTypeToChar(robot->robotType), robot->id);
+    fflush(stdout);
+#endif
+    makeChassis(robot);
+    break;
+  case BODY:
+    task->bodyIsHere = task->bodyIsHere + 1;
+    makeBody(robot);
+    task->bodyIsHere = task->bodyIsHere - 1;
+    break;
+  case WINDOW:
+    makeWindow(robot);
+    break;
+  case TIRE:
+    makeTire(robot);
+    break;
+  case BATTERY:
+    makeBattery(robot);
+    break;
+  case CAR:
+    makeCar(robot);
+    break;
+  default:
+    err_printf(__func__, __LINE__, "Error!! Robot%c[%d] gets invalid jobID %d\n", 
+        RobotTypeToChar(robot->robotType), robot->id, jobID);
+    break;
+  }
+  timer = getCurrentTime() - timer;
+#ifdef DEBUG
+  debug_printf(__func__, "Robot%c[%d] job %d done! Time: %lf\n", 
+      RobotTypeToChar(robot->robotType), robot->id, jobID, timer);
+#endif
+}
+
